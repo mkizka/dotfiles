018c7086e12a75b1809e83bef067cd5dcbb2ea57
mkizka <g.mkizka@gmail.com>
2024-12-12 00:18:56 +0900

    fix

--- home/run_install.sh
+++ home/run_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew bundle --global --quiet
 mise install
 fish -c "fisher update"
--- home/run_once_brew.sh
+++ home/run_once_brew.sh
@@ -12,5 +12,4 @@ fi
 
 if ! has brew; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
